e76ec831d6050474500174e426efb6c2cd3ec8bb
emard <vordah@gmail.com>
2020-12-28 12:22:06 +0100

    download with file replace

--- rtl/roms/getrom.sh
+++ rtl/roms/getrom.sh
@@ -5,7 +5,7 @@ ROM_CHAR=characters.901225-01.bin
 # 16384 byte file
 ROM_KERNAL=64c.251913-01.bin
 
-wget ftp://www.zimmers.net/pub/cbm/firmware/computers/c64/$ROM_CHAR
+wget -O $ROM_CHAR ftp://www.zimmers.net/pub/cbm/firmware/computers/c64/$ROM_CHAR
 ./chargen2vhdl.sh $ROM_CHAR > rom_chargen_pack_orig.vhd
-wget ftp://www.zimmers.net/pub/cbm/firmware/computers/c64/$ROM_KERNAL
+wget -O $ROM_KERNAL ftp://www.zimmers.net/pub/cbm/firmware/computers/c64/$ROM_KERNAL
 ./kernal2vhdl.sh $ROM_KERNAL > rom_kernal_pack_orig.vhd
